77630ef77873040f03723ca2d4e8aaa48403b9d8
HumbertoLuiz <kaos_132@hotmail.com>
2023-02-14 09:39:36 -0300

    hilla

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+// prettier.config.js or .prettierrc.js
+exports = {
+  trailingComma: 'all',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+}
+
+
